f4e5ca1536d4aba3d2961b952ce57f705bd67eb9
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-05-14 13:34:57 +0530

    Update cheapest_x_to_y_convert.py

--- cheapest_x_to_y_convert.py
+++ cheapest_x_to_y_convert.py
@@ -1,25 +1,27 @@
-a,b = map(str,input("").split())
-res = 0
-check = min(a,b)
-against = max(a,b)
-i = 0
-j = 0
-while(i< len(check) and j< len(against)):
-    if check[i] == against[j]:
-        i+=1
-        j+=1
+def checker(check,against):
+    i = 0
+    j = 0
+    res = 0
+    while(i< len(check) and j< len(against)):
+        if check[i] == against[j]:
+            i+=1
+            j+=1
+        else:
+            res+=1
+            j+=1
+    remi = len(check)-i
+    remj = len(against)-i
+    if(remi> remj):
+        diff = remi - remj
+        updates = remj
+    elif(remi<remj):
+        diff = remj-remi
+        updates = remi
     else:
-        res+=1
-        j+=1
-remi = len(check)-i
-remj = len(against)-i
-if(remi> remj):
-    diff = remi - remj
-    updates = remj
-elif(remi<remj):
-    diff = remj-remi
-    updates = remi
-else:
-    diff = 0
-    updates = remi
-print(diff+updates)
+        diff = 0
+        updates = remi
+    return diff+updates
+a,b = map(str,input("").split())
+res1 = checker(min(a,b),max(a,b))
+res2 = checker(max(a,b),min(a,b))
+print(min(res1,res2))
